373fcf4d51b550142031eb400aa71351c2f76dc5
User <ev9esh.mail@gmail.com>
2022-08-08 17:29:40 +0300

    add jwt

--- auth/users/views.py
+++ auth/users/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt, datetime
 
 
 class RegisterView(APIView):
@@ -11,3 +14,24 @@ class RegisterView(APIView):
         return Response(serializer.data)
 
 
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('Пользователь не найден')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=300),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        return Response({
+            'jwt': token
+        })

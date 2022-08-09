0097bc1eb9c5563c49ce6bc591884df4c5a8c5a4
User <ev9esh.mail@gmail.com>
2022-08-09 17:39:48 +0300

    add cookie

--- auth/users/views.py
+++ auth/users/views.py
@@ -32,6 +32,10 @@ class LoginView(APIView):
 
         token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
 
-        return Response({
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
             'jwt': token
-        })
+        }
+
+        return response

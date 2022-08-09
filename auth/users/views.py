85be700906d8337f96eecd0afeee0eab00cd09ff
User <ev9esh.mail@gmail.com>
2022-08-09 20:27:30 +0300

    add logout

--- auth/users/views.py
+++ auth/users/views.py
@@ -56,3 +56,13 @@ class UserView(APIView):
         serializer = UserSerializer(user)
 
         return Response(serializer.data)
+
+
+class Logout(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'Успешно'
+        }
+        return response
